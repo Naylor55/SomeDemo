6aff6be3ac1ca7bb57973192a6e35636ac352d87
陈浩楠 <3171448083@qq.com>
2017-04-24 18:26:42 +0800

    123

--- SomeDemo/MVCWebSite/Controllers/HomeController.cs
+++ SomeDemo/MVCWebSite/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace MVCWebSite.Controllers
 {
@@ -11,6 +12,57 @@ namespace MVCWebSite.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// 读取XML文件
+        /// </summary>
+        /// <returns></returns>
+        public string ReadXml()
+        {
+            string result = "失败";
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                if (System.IO.File.Exists(Server.MapPath("~/XMLFiles/ProjectConfig.xml")))
+                {
+                    XmlReader reader = XmlReader.Create(Server.MapPath("~/XMLFiles/ProjectConfig.xml"));
+                    xmlDoc.Load(reader);
+                    reader.Close();
+                    result = "成功";
+                }
+            }
+            catch (Exception e)
+            {
+                result = e.ToString();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 再次读取
+        /// </summary>
+        /// <returns></returns>
+        public string ReadXmlAgain()
+        {
+            string result = "失败";
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                if (System.IO.File.Exists(Server.MapPath("~/XMLFiles/ProjectConfig.xml")))
+                {
+                    XmlReader reader = XmlReader.Create(Server.MapPath("~/XMLFiles/ProjectConfig.xml"));
+                    xmlDoc.Load(reader);
+                    reader.Close();
+                    result = "成功";
+                }
+            }
+            catch (Exception e)
+            {
+                result = e.ToString();
+            }
+            return result;
+
         }
 
         public ActionResult About()
