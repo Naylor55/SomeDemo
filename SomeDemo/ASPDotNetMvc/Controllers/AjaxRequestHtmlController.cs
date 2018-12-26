f48b07b0cede12db67e057220ec1c8dae727f468
NaylorLeading <cnaylor@163.com>
2018-12-26 14:15:59 +0800

    update

---
+++ SomeDemo/ASPDotNetMvc/Controllers/AjaxRequestHtmlController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPDotNetMvc.Controllers
+{
+    public class AjaxRequestHtmlController : Controller
+    {
+        //
+        // GET: /AjaxRequestHtml/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
--- SomeDemo/ASPDotNetMvc/Controllers/HomeController.cs
+++ SomeDemo/ASPDotNetMvc/Controllers/HomeController.cs
@@ -21,45 +21,7 @@ namespace ASPDotNetMvc.Controllers
 
         delegate int EmailDelegate(EmailModel em);
 
-        public string SendEmail(string emailcontent)
-        {
-            string res = "cml";
-            if (!string.IsNullOrEmpty(emailcontent))
-            {
-
-                #region 委托方式创建线程来发送邮件
-                EmailDelegate ed = new EmailDelegate(EmailTool.Send);
-                EmailModel em = new EmailModel();
-                em.mailFrom = "serv1@66123123.com";
-                em.mailToArray = "3171448083@qq.com";
-                em.mailSubject = emailcontent;
-                em.mailBody = emailcontent;
-                em.mailPwd = "5v26LuSfRs";
-                em.host = "smtp.ym.163.com";
-                em.isbodyHtml = true;
-                ed.BeginInvoke(em, SendComplited, "第一封邮件");
-                #endregion
-                #region 委托方式创建线程来发送邮件
-                EmailDelegate ed2 = new EmailDelegate(EmailTool.Send);
-                EmailModel em2 = new EmailModel();
-                em2.mailFrom = "serv1@66123123.com";
-                em2.mailToArray = "3171448083@qq.com";
-                em2.mailSubject = emailcontent + "我是第二个邮件";
-                em2.mailBody = emailcontent + "我是第二个邮件";
-                em2.mailPwd = "55";
-                em2.host = "smtp.ym.163.com";
-                em2.isbodyHtml = true;
-                ed2.BeginInvoke(em2, SendComplited, "第二封邮件");
-                #endregion
-
-                res = "已经在异步发送邮件了";
-            }
-            else
-            {
-                res = "发送内容不可为空。";
-            }
-            return res;
-        }
+     
 
         private static void SendComplited(IAsyncResult result)
         {
