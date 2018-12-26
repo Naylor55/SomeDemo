b18f6ef3189e59bb46d237d91ead3d77430745f0
NaylorLeading <cnaylor@163.com>
2018-12-26 15:40:48 +0800

    添加jquery  ajax   跨域的demo

---
+++ SomeDemo/ASPDotNetMvc/Controllers/ImgBase64Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPDotNetMvc.Controllers
+{
+    public class ImgBase64Controller : Controller
+    {
+        //
+        // GET: /ImgBase64/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SomeDemo/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult Test()
+        {
+            HttpContext.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            string str = "123456";
+            return Json(str, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
